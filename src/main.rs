679f97a517f910cc2c8d7613eb17852a23ff9382
Paulo Grechi <paulo.a809@gmail.com>
2023-10-04 22:22:38 -0300

    adicionado arquivo de log

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
+use log::info;
+
+use log4rs;
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-	let path_s = std::env::args().nth(1).expect("no params");
+	log4rs::init_file("src/log.yaml", Default::default()).unwrap();
+	let path_s = std::env::args().nth(1).expect("no param");
 	let pattern = std::env::args().nth(2).expect("no param");
 	let path = std::path::PathBuf::from(path_s);
 	let content = std::fs::read_to_string(&path)?;
@@ -10,7 +15,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 fn find_pattern(content: &str, pattern: &str){
 	for line in content.lines(){
 		if line.contains(pattern){
-			println!("{}", line);
+			info!("{}", line);
 		}
-	}	
+	}
 }
