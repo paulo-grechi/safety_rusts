55ad9d9fe9da198645592df998de41726c779d06
paulo-grechi <70842539+paulo-grechi@users.noreply.github.com>
2023-11-07 10:58:50 -0300

    atualizações de busca
    alterado a verificação de se existe o padrão, para um match
    ao invés de buscar os parâmetros diretamente faz um parser deles

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,27 @@ use log::info;
 use log4rs;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-	log4rs::init_file("/home/paulo/safety_rusts/src/log.yaml", Default::default()).unwrap();
-	let path_s = std::env::args().nth(1).expect("no param");
-	let pattern = std::env::args().nth(2).expect("no param");
+	log4rs::init_file("/home/paul/safety_rusts-master/src/log.yaml", Default::default()).unwrap();
+	let args: Vec<String> = std::env::args().collect();
+	let path_s = parse_args(&args);
 	let path = std::path::PathBuf::from(path_s);
 	let content = std::fs::read_to_string(&path)?;
-	find_pattern(&content, &pattern);
+	find_pattern(&content);
 	Ok(())
 }
 
-fn find_pattern(content: &str, pattern: &str){
+fn find_pattern(content: &str){
 	for line in content.lines(){
-		if line.contains(pattern){
-			info!("{}", line);
+		match Some(line){
+			Some(x) if x.contains("rm") || x.contains("sudo") => info!("{}", line),
+			Some(_) => print!(""),
+			None => print!("")
 		}
 	}
+}
+
+fn parse_args(args: &[String]) -> &str{
+	let path_s = &args[1];
+	
+	path_s
 }
