abbaa3fdd6381feb22ce3567fb938f3c788f848d
paulo-grechi <70842539+paulo-grechi@users.noreply.github.com>
2023-11-16 15:53:30 -0300

    alterado para um método recursivo
    Buscando todos os arquivos dentro do diretório $HOME do usuário atual

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,55 @@
+
 use log::info;
 use log4rs;
+use homedir::get_my_home;
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-	log4rs::init_file("/home/paulo/safety_rusts/src/log.yaml", Default::default()).unwrap();
-	let args: Vec<String> = std::env::args().collect();
-	let path_s = parse_args(&args);
-	let path = std::path::PathBuf::from(path_s);
-	let content = std::fs::read_to_string(&path)?;
-	find_pattern(&content);
+use std::{
+	fs,
+	path::{Path,PathBuf},
+	io
+};
+
+fn main() -> Result<(), Box<dyn std::error::Error>>{
+	log4rs::init_file("./log.yaml", Default::default()).unwrap();
+	let m_path = get_my_home().unwrap().unwrap();
+	read_path(&m_path.as_path());	
 	Ok(())
 }
 
-fn find_pattern(content: &str){
+fn find_pattern(content: &str, filename: &str){
 	for line in content.lines(){
-		match Some(line){
-			Some(x) if x.contains("rm") || x.contains("sudo") => info!("{}", line),
-			Some(_) => print!(""),
-			None => print!("")
+		if has_rm_sudo(line){
+			info!("{} => {}", filename, line);
 		}
 	}
 }
 
-fn parse_args(args: &[String]) -> &str{
-	let path_s = &args[1];
-	
-	path_s
+fn folders(dir: &Path) -> Result<Vec<PathBuf>, io::Error> {
+    Ok(fs::read_dir(dir)?
+        .into_iter()
+        .filter(|r| r.is_ok())
+        .map(|r| r.unwrap().path()) // This is safe, since we only have the Ok variants
+        .filter(|r| !r.display().to_string().contains("main.rs") && !r.display().to_string().contains("requests.log"))
+        .collect())
+}
+
+fn read_path(paths: &Path){
+	for path in folders(&paths).unwrap(){
+		match fs::read_to_string(&path){
+			Ok(_) => {
+				let file = fs::read_to_string(&path).unwrap();
+				find_pattern(&file, &path.display().to_string());
+			},
+			Err(_) => {
+				if path.is_dir() {
+					read_path(&path);	
+				}
+			}
+		}
+	}
+}
+
+fn has_rm_sudo(line: &str) -> bool {
+	line.starts_with("rm") || line.contains(" rm ") || line.starts_with("sudo") || line.contains(" sudo ")
+	|| line.contains("&rm ") || line.contains("&sudo ")
 }
